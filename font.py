4168cb9710e9587c6bafda599f9710a4cb009933
Paulin <github@x1c.pw>
2025-04-24 13:44:04 +0200

    rework font logic

--- font.py
+++ font.py
@@ -1,6 +1,13 @@
+zero_width_joiner = "‍"
+
+def transpose(lists):
+    result = []
+    for x in range(len(lists[0])):
+        result.append([l[x] for l in lists])
+    return result
 
 class Pixel:
-    truthy = "░XxO"
+    truthy = "○•░XxO"
     falsy = " _-"
     def __init__(self, value):
         if value in self.truthy:
@@ -17,6 +24,10 @@ class Column:
         self.pixels = [Pixel(value) for value in pixel_str]
         self.pixels = tuple(self.pixels)
         self.height = len(self.pixels)
+    def __getitem__(self, index):
+        return self.pixels.__getitem__(index)
+    def __len__(self):
+        return len(self.pixels)
     def __repr__(self):
         chars = [str(pixel) for pixel in self.pixels]
         return "\n".join(chars)
@@ -28,9 +39,32 @@ class Character:
         if max(heights) != min(heights):
             raise ValueError("Characters must all be of the same height.")
         self.height = heights[0]
+    def __getitem__(self, index):
+        return self.columns.__getitem__(index)
+    def __add__(self, other):
+        return Character(*(self.columns + other.columns))
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
+    def __len__(self):
+        return len(self.columns)
     def __repr__(self):
-        rows = []
-        for row in range(self.height):
-            rows.append("".join([str(column.pixels[row]) for column in self.columns]))
-        return "\n".join(rows)
-            
+        rows = transpose(self.columns)
+        return rows_to_str(rows)
+    
+class Font:
+    def __init__(self, characters):
+        self.characters = characters
+    def convert(self, text):
+        text = zero_width_joiner.join(text)
+        characters = [self.characters.get(c) for c in text]
+        return sum(characters)
+    def print(self, text):
+        representation = self.convert(text)
+        rows = transpose(representation)
+        print(rows_to_str(rows))
+
+def rows_to_str(rows):
+    return "\n".join(["".join(map(str, pixel)) for pixel in rows])
--- minik_font.py
+++ minik_font.py
@@ -1,15 +1,24 @@
-from font import Column, Character
+from font import Column, Character, zero_width_joiner, Font
 
-characters = {
-    'a': Character(
-        Column("x x x "),
-        Column(" x x x")
+minik_font = Font({
+    'A': Character(
+        Column(" xxxxx"),
+        Column("x  x  "),
+        Column(" xxxxx"),
     ),
     'b': Character(
-        Column("xxxxxx")
-    )
-}
+        Column("xxxxxx"),
+        Column("   x x"),
+        Column("    x "),
+    ),
+    zero_width_joiner: Character(Column("      "), Column("      ")),
+    ' ': Character(Column("      "), Column("      ")),
+})
 
-for name, character in characters.items():
-    print(name)
-    print(character)
+for name, character in minik_font.characters.items():
+    if name not in [zero_width_joiner, ' ']:
+        print(name)
+        print(character)
+    print()
+
+minik_font.print("Ab Abbbbb")
