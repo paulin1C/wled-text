24d0e1714321b0fe860ea0ca0276e85492ca348f
Minik <66339979+LittleBlueFish@users.noreply.github.com>
2025-04-24 17:12:14 +0200

    added all the lowercase letters

--- minik_font.py
+++ minik_font.py
@@ -219,14 +219,140 @@ minik_font = Font({
 
     # lowercase letters
     'a': Character(
-        Column(""),
-        Column(""),
-        Column(""),
+        Column(" x  xx"),
+        Column(" x x x"),
+        Column("  xxxx"),
     ),
     'b': Character(
         Column("xxxxxx"),
         Column("   x x"),
         Column("    x "),
+    ),
+    'c': Character(
+        Column("  xxx "),
+        Column(" x   x"),
+        Column("  x x "),
+    ),
+    'd': Character(
+        Column("    x "),
+        Column("   x x"),
+        Column("xxxxxx"),
+    ),
+    'e': Character(
+        Column("  xxx "),
+        Column(" x x x"),
+        Column("  x  x"),
+    ),
+    'f': Character(
+        Column("  x   "),
+        Column("xxxxxx"),
+        Column("x x   "),
+    ),
+    'g': Character(
+        Column("  x  x"),
+        Column(" x x x"),
+        Column("  xxx "),
+    ),
+    'h': Character(
+        Column("xxxxxx"),
+        Column("   x  "),
+        Column("   xxx"),
+    ),
+    'i': Character(
+        Column(" x xxx"),
+    ),
+    'j': Character(
+        Column("     x"),
+        Column(" x xxx"),
+    ),
+    'k': Character(
+        Column("xxxxxx"),
+        Column("    x "),
+        Column("   x x"),
+    ),
+    'l': Character(
+        Column("xxxxx "),
+        Column("     x"),
+        Column("    x "),
+    ),
+    'm': Character(
+        Column("  xxxx"),
+        Column("   x  "),
+        Column("  x   "),
+        Column("   x  "),
+        Column("  x   "),
+        Column("   xxx"),
+    ),
+    'n': Character(
+        Column("  xxxx"),
+        Column("   x  "),
+        Column("  x   "),
+        Column("   xxx")
+    ),
+    'o': Character(
+        Column("   xx "),
+        Column("  x  x"),
+        Column("   xx "),
+    ),
+    'p': Character(
+        Column(" xxxxx"),
+        Column(" x x  "),
+        Column("  x   "),
+    ),
+    'q': Character(
+        Column("  x   "),
+        Column(" x x  "),
+        Column(" xxxxx"),
+    ),
+    'r': Character(
+        Column("  xxxx"),
+        Column("   x  "),
+        Column("  x   "),
+    ),
+    's': Character( # this is a cursive s... trust me, it is
+        Column("  x x "),
+        Column(" x   x"),
+        Column("  x  x"),
+        Column("   xx "),
+    ),     
+    't': Character(
+        Column(" x    "),
+        Column("xxxxxx"),
+        Column(" x   x"),
+    ),
+    'u': Character(
+        Column(" xxxxx"),
+        Column("     x"),
+        Column(" xxxxx"),
+    ),
+    'v': Character(
+        Column(" xxxx "),
+        Column("     x"),
+        Column(" xxxx "),
+    ),
+    'w': Character(
+        Column(" xxxx "),
+        Column("     x"),
+        Column("    x "),
+        Column("     x"),
+        Column(" xxxx "),
+    ),
+    'x': Character(
+        Column(" x   x"),
+        Column("  x x "),
+        Column("   x  "),
+        Column("  x x "),
+        Column(" x   x"),
+    ),
+    'y': Character(
+        Column(" xx   "),
+        Column("   xxx"),
+        Column(" xx   "),
+    ),
+    'z': Character(
+        Column(" x  xx"),
+        Column(" x x x"),
+        Column(" xx  x"),
     ),
 
     # special characters
@@ -248,4 +374,4 @@ for name, character in minik_font.characters.items():
         print(character)
     print()
 
-minik_font.display("16:13")
+#minik_font.display("16:29")
